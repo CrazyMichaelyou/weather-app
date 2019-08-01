e45704aedcb7c5ec440cc049b0d782976d18c0cd
Michael <2364143726@qq.com>
2019-08-01 15:12:59 +0800

    starting template

---
+++ src/app/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HomeScreen from './screens/homeScreen'
+
+class App extends React.Component {
+    render() {
+        return(<div>{HomeScreen}</div>)
+    }
+}
+
+export default App
